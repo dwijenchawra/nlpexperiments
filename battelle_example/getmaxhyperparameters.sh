efd2f827c134245887f653771c4dfbc6e06b8cfe
Dwijen Chawra <dchawra@purdue.edu>
2022-09-30 14:00:48 -0400

    finished printing script

--- battelle_example/getmaxhyperparameters.sh
+++ battelle_example/getmaxhyperparameters.sh
@@ -1,1 +1,14 @@
-for FILE in .job/out/*; do echo $FILE && cat $FILE; done | awk -
+for FILE in .job/out/*; do cat $FILE; done | awk -F ":" '
+    BEGIN{
+        PROCINFO["sorted_in"] = "@val_num_desc"
+    }
+    {
+        acc[$2] = $4;  
+    }
+    END{
+        count = 1;
+        for (i in acc) {
+            printf(i":%f\n", acc[i])
+            count++;
+        }
+    }' | head -n 10 | column --table --table-columns "Hyperparameters","Test Accuracy" -s ":"
