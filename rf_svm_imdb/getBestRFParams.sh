0f9cd0357a41ff16ae24a2aa1102dcd208666a17
Dwijen Chawra <dchawra@purdue.edu>
2022-10-04 13:18:01 -0400

    working on new params script

---
+++ rf_svm_imdb/getBestRFParams.sh
@@ -0,0 +1,14 @@
+for FILE in .job/out/*; do cat $FILE; done | awk -F ":" '
+    BEGIN{
+        PROCINFO["sorted_in"] = "@val_num_desc"
+    }
+    {
+        acc[$2] = $4;
+    }
+    END{
+        count = 1;
+        for (i in acc) {
+            printf(i":%f\n", acc[i])
+            count++;
+        }
+    }' | head -n 20 | column --table --table-columns "Hyperparameters","Test Accuracy" -s ":"
