f50d2a0b8c8db58ee9e36cec28666e7d06f20b77
Dwijen Chawra <dchawra@purdue.edu>
2022-10-15 22:26:54 -0400

    copied files

---
+++ kmeans/kmeans_jobsubmit.py
@@ -0,0 +1,78 @@
+import subprocess
+import os
+import numpy as np
+import time
+
+def mkdir_p(dir):
+    '''make a directory (dir) if it doesn't exist'''
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+
+job_directory = "%s/.rfjob" % os.getcwd()
+script_directory = "%s/.rfjob/script" % os.getcwd()
+out_directory = "%s/.rfjob/out" % os.getcwd()
+err_directory = "%s/.rfjob/err" % os.getcwd()
+
+# Make top level directories
+mkdir_p(job_directory)
+mkdir_p(script_directory)
+mkdir_p(out_directory)
+mkdir_p(err_directory)
+
+# count = 0
+# for ngmin in np.arange(1, 5, 1):
+#     for ngmax in np.arange(ngmin, 5, 1):
+#         for mindf in np.arange(0.006, 0.001, -0.001):
+#             for maxdf in [1.0]:
+#                 for c in [0.01, 0.1, 1, 10, 100]:
+#                     for penalty in ['l1', 'l2']:
+#                         count += 1
+# print(count)
+# exit()
+
+count = 0
+
+maxdf = 1.0
+ngmin = 1
+ngmax = 4
+mindf = 0.001
+for featuretype in ['bow', 'tfidf']:
+    # if count == 1:
+    #     quit()
+
+    jobname = "rf_tdm_battelle"
+    params = [ngmin, ngmax, mindf, maxdf, featuretype]
+    stringified_params = [str(i) for i in params]
+    filename = "_".join(stringified_params)
+
+    job_file = os.path.join(
+        script_directory, "%s.sh" % filename)
+    
+    out_file = os.path.join(
+        out_directory, "%s.out" % filename)
+    err_file = os.path.join(
+        err_directory, "%s.err" % filename)
+
+    # Create lizard directories
+
+    with open(job_file, "w") as fh:
+        fh.writelines("#!/bin/bash\n")
+        fh.writelines(
+            "#SBATCH --job-name=%s\n" % jobname)
+        fh.writelines(
+            "#SBATCH --output=%s\n" % out_file)
+        fh.writelines(
+            "#SBATCH --error=%s\n" % err_file)
+        fh.writelines("#SBATCH --time=2-00:00:00\n")
+        fh.writelines("#SBATCH --mem=128000\n")
+        fh.writelines("#SBATCH --cores=64\n")
+        fh.writelines("#SBATCH --account=cis220051\n")
+        fh.writelines("#SBATCH --partition=shared\n")
+        fh.writelines(
+            "python ./rfmodeloptimization.py %g %g %g %g %s %s" % (ngmin, ngmax, mindf, maxdf, featuretype, filename))
+
+    subprocess.call(['sbatch %s' % job_file], shell=True)
+    time.sleep(0.1)
+    
+    count += 1
---
+++ kmeans/kmeansmodeloptimization.py
@@ -0,0 +1,196 @@
+import numpy as np
+import re
+
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize, wordpunct_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from string import punctuation
+from prompt_toolkit import print_formatted_text
+from pytest import param
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV, train_test_split, GridSearchCV
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
+
+'''
+tell people about what the confusion matrix means
+what parts are important to us
+'''
+
+import sys
+
+ngram_min, ngram_max, min_df, max_df, featuretype, filename = sys.argv[1:]
+
+
+data_path = "/home/x-dchawra/nlpexperiments/rf_svm_imdb/IMDB Dataset.csv.zip"
+
+
+data = pd.read_csv(data_path)
+data.columns = ['text', 'label']
+data['label'] = data.label.map({'negative': 0, 'positive': 1})
+
+wnl = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+punct_remover = str.maketrans('', '', punctuation)
+
+
+def preprocess_text(text):
+    text = re.sub('<.*?>', '', text)
+    tokens = wordpunct_tokenize(text.lower())
+    tokens = [i.translate(punct_remover) for i in tokens]
+    tokens = [i for i in tokens if i not in stop_words]
+    tokens = [wnl.lemmatize(i) for i in tokens]
+    text = " ".join(tokens)
+
+    return text
+
+
+text_data = []
+for i in data.text:
+    text_data.append(preprocess_text(i))
+data['text'] = text_data
+
+# avg_tokens = sum([len(i.split()) for i in data.text]) / len(data.text)
+# print(f"Average Number of Tokens per Review: {round(avg_tokens)}")
+
+
+def generate_features(data, test_size=0.2, feature_type='bow', ngram_min=1, ngram_max=1, min_df=1, max_df=1.0):
+    """
+    Function to generate features for train and test data
+
+    Parameters
+    ----------
+    data - Dataframe with columns 'text' and 'label'
+    test_size - Proportion of data that will be held out for evaluation
+    feature_type - Type of features to use as represenation. Options are 'bow' or 'tfidf'
+    max_df - When building the vocabulary ignore terms that have a document frequency strictly
+             higher than the given threshold (corpus-specific stop words). If float, the parameter
+             represents a proportion of documents, integer absolute counts. This parameter is ignored
+             if vocabulary is not None.
+    min_df - When building the vocabulary ignore terms that have a document frequency strictly lower than
+            the given threshold. If float, the parameter represents a proportion of documents, integer
+            absolute counts. This parameter is ignored if vocabulary is not None.
+
+    Returns
+    -------
+    x_train - Input features for the train set
+    y_train - Labels for the train set
+    x_test - Input features for the test set
+    y_test - Labels for the test set
+    """
+
+    # Split data into Train and Test
+    train_df, test_df = train_test_split(data, test_size=0.2)
+
+    # Instantiate Object to generate feature representations
+    if feature_type == 'bow':
+        vectorizer = CountVectorizer(ngram_range=(
+            ngram_min, ngram_max), min_df=min_df, max_df=max_df)
+    elif feature_type == 'tfidf':
+        vectorizer = TfidfVectorizer(ngram_range=(
+            ngram_min, ngram_max), min_df=min_df, max_df=max_df)
+    else:
+        raise ValueError(
+            "feature_type must be set to either 'bow' or 'tfidf'.")
+
+    # Generate features for train and test set
+    x_train = vectorizer.fit_transform(train_df.text)
+    x_test = vectorizer.transform(test_df.text)
+    y_train = train_df.label.values
+    y_test = test_df.label.values
+
+    return x_train, y_train, x_test, y_test
+
+
+def optimizeSVMModel(ngram_min, ngram_max, min_df, max_df, feature_type='bow'):
+    xtrain, ytrain, xtest, ytest = generate_features(
+        data, feature_type=feature_type, ngram_min=ngram_min, ngram_max=ngram_max, min_df=min_df, max_df=max_df
+    )
+
+    # Define model parameters and train model
+    grid = GridSearchCV(LinearSVC(penalty='l2', C=10, tol=1e-5,
+                        max_iter=5000), parameters, n_jobs=-1)
+    grid.fit(xtrain, ytrain)
+
+    best_model = grid.best_estimator_
+    best_test_acc = best_model.score(xtest, ytest)
+
+    # Score Model and Generate confusion matrix
+    test_pred = best_model.predict(xtest)
+    test_prec = precision_score(ytest, test_pred)
+    test_recall = recall_score(ytest, test_pred)
+    test_f1 = f1_score(ytest, test_pred)
+
+    # Generate confusion matrix
+    # conf_matrix = confusion_matrix(ytest, test_pred, normalize='true')
+    # ConfusionMatrixDisplay(conf_matrix, display_labels=[0, 1]).plot()
+
+    return best_test_acc, test_prec, test_recall, test_f1
+
+
+def optimizeRFModel(ngram_min, ngram_max, min_df, max_df, feature_type='bow'):
+
+    xtrain, ytrain, xtest, ytest = generate_features(
+        data, feature_type=feature_type, ngram_min=ngram_min, ngram_max=ngram_max, min_df=min_df, max_df=max_df
+    )
+    # n_estimators = [10]
+    # # Number of features to consider at every split
+    # max_features = ['sqrt']
+    # # Maximum number of levels in tree
+    # max_depth = [int(x) for x in np.arange(1, 20, step=5)]
+    # # Minimum number of samples required to split a node
+    # min_samples_split = [2]
+    # # Minimum number of samples required at each leaf node
+    # min_samples_leaf = [2]
+
+    # testing cv
+    # Number of trees in random forest
+    n_estimators = [int(x) for x in np.linspace(start=50, stop=2000, num=50)] # 2nd run: 50-1000?
+    # Maximum number of levels in tree
+    max_depth = [int(x) for x in np.linspace(1, 50, num=5)] # 2nd run: max < 110
+    max_depth.append(None)
+    # Minimum number of samples required to split a node
+    min_samples_split = [2, 5, 10]
+    # Minimum number of samples required at each leaf node
+    min_samples_leaf = [1, 2]
+    # Create the random grid
+    random_grid = {'n_estimators': n_estimators,
+                'max_depth': max_depth,
+                'min_samples_split': min_samples_split,
+                'min_samples_leaf': min_samples_leaf
+                }
+
+    grid = GridSearchCV(RandomForestClassifier(), param_grid=random_grid,
+        scoring=["accuracy", "f1", "precision", "recall", "roc_auc"], n_jobs=64, verbose=1, refit=False, return_train_score=True)
+    grid.fit(xtrain, ytrain)
+
+    results = pd.DataFrame(grid.cv_results_)
+    results.to_csv("/home/x-dchawra/nlpexperiments/rf_svm_imdb/.rfjob/out/" + filename + ".csv")
+    
+
+    # best_model = grid.best_estimator_
+    # best_params = grid.best_params_
+    # best_test_acc = best_model.score(xtest, ytest)
+
+    # # Score Model and Generate confusion matrix
+    # test_pred = best_model.predict(xtest)
+    # test_prec = precision_score(ytest, test_pred)
+    # test_recall = recall_score(ytest, test_pred)
+    # test_f1 = f1_score(ytest, test_pred)
+
+    # return best_test_acc, test_prec, test_recall, test_f1, best_params
+    
+
+# job scheduler
+# best_test_acc, test_prec, test_recall, test_f1, params = optimizeRFModel(int(ngram_min), int(ngram_max), np.double(min_df), np.double(max_df), str(featuretype))
+
+optimizeRFModel(int(ngram_min), int(ngram_max), np.double(min_df), np.double(max_df), str(featuretype))
+
+# print(f"NLPParams;{filename};RFParams;{params};BestTestAccuracy;{best_test_acc};Precision;{test_prec};Recall;{test_recall};F1;{test_f1}")
+
+# print(f"{filename},{params['n_estimators']},{params['min_samples_split']},{params['min_samples_leaf']},{params['max_features']},{params['max_depth']},{params['bootstrap']},{best_test_acc},{test_prec},{test_recall},{test_f1}")
+
+
